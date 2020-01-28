e9f3b83b54a2f1243def82b2c4899f8d9b4249c5
Gabriel Killy <gabby.killy@gmail.com>
2020-01-28 16:05:54 -0600

    p absolute footer for about page

--- partials/footer.php
+++ partials/footer.php
@@ -1,3 +1,3 @@
-<div class="<?=(($page_name == "contact") ? "position-absolute w-100" : "")." footer"?>">
+<div class="<?=((in_array($page_name, ["contact", "about"])) ? "position-absolute w-100" : "")." footer"?>">
   <small class="pt-3">Made by <a href="https://www.nayeliperez.com">Nayeli Pérez</a>  |  Copyright 2020</small>
 </div>
