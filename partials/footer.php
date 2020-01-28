8ad2f93b221c50adade626e86f6653a9357478b7
Nayeli Perez <nayeliaperezt@gmail.com>
2020-01-28 15:53:44 -0600

    Add's fix for contact page footer

--- partials/footer.php
+++ partials/footer.php
@@ -1,3 +1,3 @@
-<div class="footer">
+<div class="<?=(($page_name == "contact") ? "position-absolute w-100" : "")." footer"?>">
   <small class="pt-3">Made by <a href="https://www.nayeliperez.com">Nayeli Pérez</a>  |  Copyright 2020</small>
 </div>
