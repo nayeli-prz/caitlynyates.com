9764dd4fec1efd179cf70ae296d9acf0446acece
Nayeli Perez <nayeliaperezt@gmail.com>
2020-08-06 17:21:51 -0500

    Quick typo fix

--- about.php
+++ about.php
@@ -10,7 +10,7 @@
       </div>
       <img class="rounded-lg headshot mb-4 shadow" src="assets/images/caitlyn_3.jpg" alt="Headshot of Caitlyn">
       <p class="about">
-        Caitlyn Yates is a PhD student in socio-cultural anthropology at the University of British Columbia. Prior to UBC, Yates worked as a researcher at the the Strauss Center for International Security and Law, IBI Consultants, the Institute for National Strategic Studies. Her work focuses on human security, organized crime, transit migration, migrant facilitation and borders in Latin America. Caitlyn focuses specifically on Mexico and the Colombia-Panama border in her fieldwork. She has presented her work before the United Nations, United States government agencies and departments, and at academic conferences. Yates holds a Bachelor of Arts from Trinity University and a Master of Global Policy Studies from the LBJ School of Public Affairs at the University of Texas at Austin.
+        Caitlyn Yates is a PhD student in socio-cultural anthropology at the University of British Columbia. Prior to UBC, Yates worked as a researcher at the Strauss Center for International Security and Law, the Institute for National Strategic Studies, and IBI Consultants. Her work focuses on human security, organized crime, transit migration, migrant facilitation and borders in Latin America. Caitlyn focuses specifically on Mexico and the Colombia-Panama border in her fieldwork. She has presented her work before the United Nations, United States government agencies and departments, and at academic conferences. Yates holds a Bachelor of Arts from Trinity University and a Master of Global Policy Studies from the LBJ School of Public Affairs at the University of Texas at Austin.
       </p>
     </div>
   </div>
